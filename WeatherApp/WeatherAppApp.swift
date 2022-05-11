e7b36693b454862879831a28ae3c6c8b4ee1b03b
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-11 17:40:35 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
